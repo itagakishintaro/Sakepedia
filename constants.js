b01e5a2861c3f3509d1e139495832c16b4cba8b0
itagaki <itagaki.shintaro@gmail.com>
2016-10-18 00:24:08 +0900

    Change constant

--- constants.js
+++ constants.js
@@ -1,10 +1,10 @@
 const constants = {};
 
-// constants.host = 'http://localhost:3000';
+constants.host = 'http://localhost:3000';
 // constants.host = 'http://192.168.179.3:3000';
-constants.host = 'https://sakepedia.herokuapp.com';
+// constants.host = 'https://sakepedia.herokuapp.com';
 
-// constants.mongourl = 'mongodb://localhost:27017/Sakepedia';
-constants.mongourl = 'mongodb://heroku_zvpkmx6c:of08mhf44rloda0bcni3h8tjq6@ds011495.mlab.com:11495/heroku_zvpkmx6c';
+constants.mongourl = 'mongodb://localhost:27017/Sakepedia';
+// constants.mongourl = 'mongodb://heroku_zvpkmx6c:of08mhf44rloda0bcni3h8tjq6@ds011495.mlab.com:11495/heroku_zvpkmx6c';
 
 module.exports = constants;
