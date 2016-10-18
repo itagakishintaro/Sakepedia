7a415d0b351a714d1b1d6dba0f9a1beb5b49ab11
itagaki <itagaki.shintaro@gmail.com>
2016-10-18 23:00:07 +0900

    close #27

--- public/app.js
+++ public/app.js
@@ -1,5 +1,5 @@
 if('serviceWorker' in navigator) {
   navigator.serviceWorker
-           .register('/service-worker.js')
-           .then(function() { console.log('Service Worker Registered'); });
+    .register('/service-worker.js')
+    .then(function() { console.log('Service Worker Registered'); });
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -58,11 +58,18 @@ class Header extends React.Component{
           onTitleTouchTap={ this.toHome }
           style={ styles.appbar }
         />
-      <Drawer open={this.state.open} docked={false} onRequestChange={ this.close }>
+        <Drawer open={this.state.open} docked={false} onRequestChange={ this.close }>
           <Link to={'/'} style={styles.link}><MenuItem onTouchTap={ this.close }>トップ</MenuItem></Link>
           <Link to={'/sake/new'} style={styles.link}><MenuItem onTouchTap={ this.close } disabled={!this.props.isLogin}>銘柄登録</MenuItem></Link>
           <Link to={'/login'} style={styles.link} onTouchTap={this.preventWhenLoggedin}><MenuItem onTouchTap={ this.close } disabled={this.props.isLogin}>ログイン</MenuItem></Link>
           <MenuItem href='/auth/logout' disabled={!this.props.isLogin} onTouchTap={ this.logout }>ログアウト</MenuItem>
+          <MenuItem><a href="https://twitter.com/share"
+            className="twitter-share-button"
+            data-lang="ja"
+            data-hashtags="sakepedia"
+          >
+            Tweet
+          </a></MenuItem>
         </Drawer>
       </div>
     )
