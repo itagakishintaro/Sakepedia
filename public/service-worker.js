e8f5cd14290d47e4e721742dae39e1c71f40fc98
itagaki <itagaki.shintaro@gmail.com>
2016-10-18 21:28:40 +0900

    Set favicon

---
+++ public/service-worker.js
@@ -0,0 +1,57 @@
+// https://developers.google.com/web/fundamentals/getting-started/codelabs/your-first-pwapp/
+
+var dataCacheName = 'Sakepedia-v1';
+var cacheName = 'Sakepedia';
+var filesToCache = [
+  '/',
+  '/app.js',
+  '/javascripts/bundle.js',
+  '/javascripts/bundle.js.map'
+];
+
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener('activate', function(e) {
+  console.log('[ServiceWorker] Activate');
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        console.log('[ServiceWorker] Removing old cache', key);
+        if (key !== cacheName) {
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+});
+
+self.addEventListener('fetch', function(e) {
+  console.log('[ServiceWorker] Fetch', e.request.url);
+  var dataUrl = 'https://publicdata-weather.firebaseio.com/';
+  if (e.request.url.indexOf(dataUrl) === 0) {
+    e.respondWith(
+      fetch(e.request)
+        .then(function(response) {
+          return caches.open(dataCacheName).then(function(cache) {
+            cache.put(e.request.url, response.clone());
+            console.log('[ServiceWorker] Fetched&Cached Data');
+            return response;
+          });
+        })
+    );
+  } else {
+    e.respondWith(
+      caches.match(e.request).then(function(response) {
+        return response || fetch(e.request);
+      })
+    );
+  }
+});
