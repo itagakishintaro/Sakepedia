6b13ed4c9cc0a1b6c26b4b3c78ef73eebbb42f1b
itagaki <itagakishintaro@jp.fujitsu.com>
2016-10-31 11:09:02 +0900

    Change data cashe strategy network then cashe

--- src/actions/sake.js
+++ src/actions/sake.js
@@ -43,17 +43,13 @@ export const setRices = ( rices ) => {
   }
 }
 
-const handleCashe = ( url, hasRequestPending, func ) => {
+const handleCashe = ( url, func ) => {
   if ( 'caches' in window ) {
     caches.match( url ).then( response => {
       if ( response ) {
         response.json().then( json => {
-          // Only update if the XHR is still pending, otherwise the XHR
-          // has already returned and provided the latest data.
-          if ( hasRequestPending ) {
-            console.log( 'get from cache' )
-            func( json )
-          }
+          console.log( 'get from cache' )
+          func( json )
         })
       }
     })
@@ -72,86 +68,66 @@ export const getSakeList = ( dispatch, words ) => {
     query = `${query}&brand=${b64encode(words.brand)}`
   }
   let url = `/api/sakes?${query}`
-
-  let hasRequestPending = true
-  handleCashe(url, hasRequestPending, ( data ) => { dispatch( setSakeList( data ) ) })
-
   axios.get( url )
     .then( res => {
-      hasRequestPending = false
       dispatch( setSakeList( res.data ) )
     })
     .catch( error => {
-      console.log( error )
+      handleCashe(url, ( data ) => { dispatch( setSakeList( data ) ) })
     })
 }
 
 export const getSake = ( dispatch, id ) => {
   let url = `/api/sakes/${id}`
-  let hasRequestPending = true
-  handleCashe(url, hasRequestPending, ( data ) => { dispatch( setSake( data ) ) })
   axios.get( url )
     .then( res => {
-      hasRequestPending = false
       dispatch( setSake( res.data ) )
     })
     .catch( error => {
-      console.log( error )
+      handleCashe(url, ( data ) => { dispatch( setSake( data ) ) })
     })
 }
 
 export const getBrands = ( dispatch ) => {
   let url = '/api/sakes/brands'
-  let hasRequestPending = true
-  handleCashe(url, hasRequestPending, ( data ) => { dispatch( setBrands( data ) ) })
   axios.get( url )
     .then( res => {
-      hasRequestPending = false
       dispatch( setBrands( res.data ) )
     })
     .catch( error => {
-      console.log( error )
+      handleCashe(url, ( data ) => { dispatch( setBrands( data ) ) })
     })
 }
 
 export const getBreweries = ( dispatch ) => {
   let url = '/api/sakes/breweries'
-  let hasRequestPending = true
-  handleCashe(url, hasRequestPending, ( data ) => { dispatch( setBreweries( data ) ) })
   axios.get( url )
     .then( res => {
-      hasRequestPending = false
       dispatch( setBreweries( res.data ) )
     })
     .catch( error => {
-      console.log( error )
+      handleCashe(url, ( data ) => { dispatch( setBreweries( data ) ) })
     })
 }
 
 export const getSakeYeasts = ( dispatch ) => {
   let url = '/api/sakes/sakeYeasts'
-  let hasRequestPending = true
-  handleCashe(url, hasRequestPending, ( data ) => { dispatch( setSakeYeasts( data ) ) })
   axios.get( url )
     .then( res => {
-      hasRequestPending = false
       dispatch( setSakeYeasts( res.data ) )
     })
     .catch( error => {
-      console.log( error )
+      handleCashe(url, ( data ) => { dispatch( setSakeYeasts( data ) ) })
     })
 }
 
 export const getRices = ( dispatch ) => {
   let url = '/api/sakes/rices'
-  let hasRequestPending = true
-  handleCashe(url, hasRequestPending, ( data ) => { dispatch( setRices( data ) ) })
   axios.get( url )
     .then( res => {
-      hasRequestPending = false
       dispatch( setRices( res.data ) )
     })
     .catch( error => {
-      console.log( error )
+      handleCashe(url, ( data ) => { dispatch( setRices( data ) ) })
     })
 }
